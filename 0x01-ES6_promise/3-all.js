15be948b05c333935c5a02916ed13e25b728b69e
Duaa Rabie <duaarabie11@gmail.com>
2024-10-02 22:34:11 +0000

    update code

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,10 +6,8 @@ function handleProfileSignup() {
     createUser(),
   ]).then(([photoData, userData]) => {
     console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
-    return photoData;
   }).catch((error) => {
     console.log('Signup system offline');
-    throw error;
   });
 }
 export default handleProfileSignup;
