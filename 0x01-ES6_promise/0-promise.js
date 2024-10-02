2b4ac8c376df83ecd9ccf24605ad5ddd15568f57
Duaa Rabie <duaarabie11@gmail.com>
2024-10-02 18:00:32 +0000

    update code

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 export async function getResponseFromAPI() {
-  return "Response";
+  // Simulate an API call
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const apiResponse = { message: "Hello from API!" };
+      resolve(apiResponse);
+    }, 1000); // Simulate a delay
+  });
 }
+
