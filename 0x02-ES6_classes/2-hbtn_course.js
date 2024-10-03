4acd20b01d4072878fd098820e2a717db9dc1ba3
Duaa Rabie <duaarabie11@gmail.com>
2024-10-03 22:30:54 +0000

    update code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,26 +1,23 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._validateName(name);
-    this._validateLength(length);
-    this._validateStudents(students);
     this._name = name;
     this._lenght = length;
     this._students = students;
   }
 
-  _validateName(name) {
+  static _validateName(name) {
     if (typeof name !== 'string' || name.length === 0) {
       throw new TypeError('Name must be a string');
     }
   }
 
-  _validateLength(length) {
+  static _validateLength(length) {
     if (typeof length !== 'number' || length <= 0) {
       throw new TypeError('Length must be a number');
     }
   }
 
-  _validateStudents(students) {
+  static _validateStudents(students) {
     if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
     }
@@ -34,7 +31,7 @@ export default class HolbertonCourse {
   }
 
   set name(value) {
-    this._validateName(value);
+    this.constructor._validateName(value);
     this._name = value;
   }
 
@@ -43,7 +40,7 @@ export default class HolbertonCourse {
   }
 
   set length(value) {
-    this._validateLength(value);
+    this.constructor._validateLength(value);
     this._length = value;
   }
 
@@ -52,7 +49,7 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    this._validateStudents(value);
+    this.constructor._validateStudents(value);
     this._students = [...value];
   }
 }
