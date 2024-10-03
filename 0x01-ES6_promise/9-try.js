73bdc14bbb739a57cca6532c29b13e516bfce2bf
Duaa Rabie <duaarabie11@gmail.com>
2024-10-03 00:09:30 +0000

    update code

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,23 +1,23 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  
+
   try {
     // Execute the mathFunction and store the result
     const result = mathFunction();
-    
+
     // Append the result to the queue
     queue.push(result);
-    
+
     // Always append "Guardrail was processed"
     queue.push('Guardrail was processed');
   } catch (error) {
     // If an error occurs, append the error message to the queue
-    const errorMessage = 'Error: ' + error.message
+    const errorMessage = `Error: ${error.message}`;
     queue.push(errorMessage);
-    
+
     // Always append "Guardrail was processed"
     queue.push('Guardrail was processed');
   }
-  
+
   return queue;
 }
