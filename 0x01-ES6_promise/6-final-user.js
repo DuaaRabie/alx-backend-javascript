e700163d31487231855f91bb332351578bc3d503
Duaa Rabie <duaarabie11@gmail.com>
2024-10-03 00:54:18 +0000

    update code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,27 +5,23 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then(([signData, photoData]) => {
-    return [
-      {
-        status: 'success',
-        value: signData.body
-      },
-      {
-        status: 'success',
-        value: photoData.body
-      }
-    ];
-  }).catch(error => {
-    return [
-      {
-        status: 'error',
-        value: error.message || 'An unknown error occurred'
-      },
-      {
-        status: 'error',
-        value: error.message || 'An unknown error occurred'
-      }
-    ];
-  });
+  ]).then(([signData, photoData]) => [
+    {
+      status: 'success',
+      value: signData.body,
+    },
+    {
+      status: 'success',
+      value: photoData.body,
+    },
+  ]).catch((error) => [
+    {
+      status: 'error',
+      value: error.message || 'An unknown error occurred',
+    },
+    {
+      status: 'error',
+      value: error.message || 'An unknown error occurred',
+    },
+  ]);
 }
