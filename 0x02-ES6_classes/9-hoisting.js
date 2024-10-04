ef3d833355632d4421e47d4af2e1986a859b0b3b
Duaa Rabie <duaarabie11@gmail.com>
2024-10-04 02:25:51 +0000

    task10

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,18 @@
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -18,13 +33,22 @@ export class StudentHolberton {
   }
 }
 
+
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+// Create StudentHolberton instances
+export const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+export const student2 = new StudentHolberton('John', 'Doe', class2020);
+export const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+export const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+export const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+// Export the array of students
+export const listOfStudents = [
+  student1,
+  student2,
+  student3,
+  student4,
+  student5
+];
