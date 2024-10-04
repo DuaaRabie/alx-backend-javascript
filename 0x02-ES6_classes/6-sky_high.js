b2d4cff6d5750f4a9eff5fbbda675b7a581c3b33
Duaa Rabie <duaarabie11@gmail.com>
2024-10-04 01:54:23 +0000

    task6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (value < 0) {
+      throw new Error('Square feet must be a positive number');
+    }
+    this._sqft = value;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
