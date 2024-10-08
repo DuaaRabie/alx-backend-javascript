e6cfbbd313c79d0f059dab77f5db60ef5d16f528
Duaa Rabie <duaarabie11@gmail.com>
2024-10-08 22:58:38 +0000

    update code

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!(arg instanceof Map)) {
+    throw new Error("Cannot procss");
+  }
   return map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
