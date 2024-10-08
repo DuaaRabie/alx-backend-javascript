3389fd263e67fcb7cb33dad23e280ee776e4c835
Duaa Rabie <duaarabie11@gmail.com>
2024-10-08 21:32:10 +0000

    update code

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(array, city, newGrades) {
   const updateArray = array.map((student) => {
     const gradeObj = newGrades.find((obj) => obj.studentId === student.id);
-    return gradeObj ? { ...student, grade: gradeObj.grade } : student;
+    return gradeObj ? { ...student, grade: gradeObj.grade } : { ...student, grade: 'N/A' };
   });
 
   return updateArray.filter((obj) => obj.location === city);
