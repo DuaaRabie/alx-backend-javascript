02c3b97cbb58303fa9877332c229a33c2eaa38e6
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-26 10:26:32 +0000

    update code

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,9 @@
+#!/usr/bin/node
+//Define the function to display a message
+
 const displayMessage = (message) => {
-  // display the message
-  console.log(message);
+    // display the message
+    console.log(message);
 }
 
 module.exports = displayMessage;
