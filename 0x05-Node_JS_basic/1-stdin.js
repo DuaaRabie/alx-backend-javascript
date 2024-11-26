9819abe10f0509405a93fe16293ac97e83969ceb
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-26 11:44:53 +0000

    update code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.resume();
 process.stdin.on('data', function(data) {
-  var name = data.toString().trim();
+  const name = data.toString().trim();
   console.log("Your name is: %s", name);
   process.stdin.pause(); 
 });
 process.stdin.on('end', function() {
-  process.stdout.write("This important software is now closing\n");
+  console.log("This important software is now closing");
 });
