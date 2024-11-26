9c79c8a7dd0450c49e6720fc655184b381520901
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-26 00:30:05 +0000

    update code

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-// console.js:wq
+
 
 function displayMessage(message) {
     console.log(message);
 }
+
 
 module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,2 @@
+message = "Welcome to Holberton School, what is your name?"
+console.log(message + '\n')
