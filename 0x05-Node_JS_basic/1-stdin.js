a5e1227bec294a219ad1c10d0846edb7f1235633
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-26 11:53:44 +0000

    update code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
 // 1. Using Process stdin
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
-process.stdin.resume();
-process.stdin.on('data', function(data) {
-  const name = data.toString().trim();
-  console.log("Your name is: %s", name);
-  process.stdin.pause(); 
+process.stdin.resume()
+process.stdin.on('readable', function() {
+  var name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 process.stdin.on('end', function() {
   console.log("This important software is now closing");
