06d4684dd550267324f08f3bd31d01c4a3a83f96
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-27 00:26:31 +0000

    task3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,12 +1,26 @@
-//3. Reading a file asynchronously with Node JS
-function countStudents(path) {
-  const fs = require('fs');  
-  try:
-  {
-    const file = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n') - 1;
+// 2. Reading a file synchronously with Node JS
+const fs = require('fs');
 
-    if file {
-      
+function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, file) => {
+    if (err) throw Error('Cannot load the database');
+    const lines = file.trim().split('\n');
+    const students = lines.slice(1).map((line) => line.split(','));
+    const fieldCount = {};
+
+    // eslint-disable-next-line no-unused-vars
+    students.forEach(([firstName, lastName, age, field]) => {
+      if (!fieldCount[field]) {
+        fieldCount[field] = [];
+      }
+      fieldCount[field].push(firstName);
+    });
+    console.log(`Number of students: ${students.length}`);
+    /* eslint-disable-next-line guard-for-in */
+    for (const field in fieldCount) {
+      console.log(`Number of students in ${field}: ${fieldCount[field].length}. List: ${fieldCount[field].join(', ')}`);
     }
+  });
 }
+
+module.exports = countStudents;
