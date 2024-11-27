5d20c5c731543e7cd48108d5df539ed75f997127
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-27 00:51:07 +0000

    task3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,24 +2,29 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, file) => {
-    if (err) throw Error('Cannot load the database');
-    const lines = file.trim().split('\n');
-    const students = lines.slice(1).map((line) => line.split(','));
-    const fieldCount = {};
-
-    // eslint-disable-next-line no-unused-vars
-    students.forEach(([firstName, lastName, age, field]) => {
-      if (!fieldCount[field]) {
-        fieldCount[field] = [];
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, file) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
       }
-      fieldCount[field].push(firstName);
+      const lines = file.trim().split('\n');
+      const students = lines.slice(1).map((line) => line.split(','));
+      const fieldCount = {};
+
+      // eslint-disable-next-line no-unused-vars
+      students.forEach(([firstName, lastName, age, field]) => {
+        if (!fieldCount[field]) {
+          fieldCount[field] = [];
+        }
+        fieldCount[field].push(firstName);
+      });
+      console.log(`Number of students: ${students.length}`);
+      /* eslint-disable-next-line guard-for-in */
+      for (const field in fieldCount) {
+        console.log(`Number of students in ${field}: ${fieldCount[field].length}. List: ${fieldCount[field].join(', ')}`);
+      }
+      resolve();
     });
-    console.log(`Number of students: ${students.length}`);
-    /* eslint-disable-next-line guard-for-in */
-    for (const field in fieldCount) {
-      console.log(`Number of students in ${field}: ${fieldCount[field].length}. List: ${fieldCount[field].join(', ')}`);
-    }
   });
 }
 
