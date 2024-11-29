8ac3e78db1709578152b4ac376610e5cf1c9d8fc
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-29 00:45:49 +0000

    task8, task9

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Route for the index page
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+// Start the server
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;  // Export the app instance for testing
