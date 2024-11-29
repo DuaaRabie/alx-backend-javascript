d3ac769c8662778e938e594bddb9b93b13602194
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-29 00:55:50 +0000

    update code

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 
-// Define a route for the index page
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the payment system');
-});
-
-// Start the server and listen on port 7865
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
 
-module.exports = app; // Export the app to be used in tests
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,43 +1,36 @@
+const request = require('request');
 const chai = require('chai');
 const expect = chai.expect;
-const request = require('request');
-const app = require('./api.js'); // Import the Express app
 
-let server;
-
-describe('Index page', function () {
-  // "before all" hook to start the server before tests run
-  before(function (done) {
-    // Start the server only once before tests
-    server = app.listen(7865, () => {
-      console.log('API available on localhost port 7865');
-      done(); // Notify Mocha that the server has started
+describe('API Integration Test', () => {
+  it('Should receive a successful response from the API', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.include('Welcome to the payment system');
+      done();
     });
   });
 
-  // "after all" hook to stop the server after tests complete
-  after(function (done) {
-    if (server) {
-      server.close(done); // Close the server if it exists
-    } else {
-      done(); // If the server is not running, just call done to proceed
-    }
-  });
-
-  // Test to check if the correct content type is returned
-  it('should have the correct content type', function (done) {
-    request('http://localhost:7865', function (error, response, body) {
-      expect(response.statusCode).to.equal(200);  // Check the status code is 200
-      expect(response.headers['content-type']).to.include('text/html');  // Check content type
-      done(); // Mark the test as completed
+  it('Should receive a correct status code', (done) => {
+    request.get('http://localhost:7865/', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
   });
 
-  // Test to check if the correct message is returned in the response body
-  it('should return correct message', function (done) {
-    request('http://localhost:7865', function (error, response, body) {
-      expect(body).to.include('Welcome to the payment system');  // Check response body
-      done(); // Mark the test as completed
+  it('Should receive the expected result', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
+      expect(body).to.include('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Should have a running server', (done) => {
+    request.get('http://localhost:7865/', (error, response) => {
+      if (error) {
+        throw new Error('Server is not running');
+      }
+      done();
     });
   });
 });
